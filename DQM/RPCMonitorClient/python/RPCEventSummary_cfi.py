6a2d144a806f1c9c70d01155775c1c5dde820994
acimmino <anna.cimmino@cern.ch>
2014-03-18 10:32:53 +0100

    version 3

---
+++ DQM/RPCMonitorClient/python/RPCEventSummary_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+rpcEventSummary = cms.EDAnalyzer("RPCEventSummary",
+                                 EventInfoPath = cms.untracked.string('RPC/EventInfo'),
+                                 PrescaleFactor = cms.untracked.int32(5),
+                                 MinimumRPCEvents = cms.untracked.int32(10000),
+                                 EnableEndcapSummary = cms.untracked.bool(True),
+                                 OfflineDQM = cms.untracked.bool(True),
+                                 RecHitTypeFolder = cms.untracked.string("AllHits")
+                                 )
